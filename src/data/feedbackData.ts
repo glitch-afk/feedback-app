75c8998de847e8134ebccd9a46332c93b2f8c00e
v3n0m <rahul004@protonmail.ch>
2022-02-18 15:44:04 +0530

    .

--- src/data/feedbackData.ts
+++ src/data/feedbackData.ts
@@ -1,27 +1,27 @@
 export const feedbackData = [
-    {
-      id: 1,
-      rating: 8,
-      text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat impedit Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat impedit Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat impedit Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat impedit",
-    },
-    {
-      id: 2,
-      rating: 7,
-      text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat impedit Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat impedit Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat impedit Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat impedit",
-    },
-    {
-      id: 3,
-      rating: 9,
-      text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat impedit Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat impedit Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat impedit Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat impedit",
-    },
-    {
-      id: 4,
-      rating: 8,
-      text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat impedit Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat impedit Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat impedit Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat impedit",
-    },
-    {
-      id: 5,
-      rating: 10,
-      text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat impedit Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat impedit Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat impedit Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat impedit",
-    },
-  ]
+  {
+    id: 1,
+    rating: 8,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, vero dolorum. Delectus harum assumenda voluptate ipsum ipsa enim modi ipsam optio officiis inventore? Enim, vel!",
+  },
+  {
+    id: 2,
+    rating: 7,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, vero dolorum. Delectus harum assumenda voluptate ipsum ipsa enim modi ipsam optio officiis inventore? Enim, vel!",
+  },
+  {
+    id: 3,
+    rating: 9,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, vero dolorum. Delectus harum assumenda voluptate ipsum ipsa enim modi ipsam optio officiis inventore? Enim, vel!",
+  },
+  {
+    id: 4,
+    rating: 8,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, vero dolorum. Delectus harum assumenda voluptate ipsum ipsa enim modi ipsam optio officiis inventore? Enim, vel!",
+  },
+  {
+    id: 5,
+    rating: 10,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, vero dolorum. Delectus harum assumenda voluptate ipsum ipsa enim modi ipsam optio officiis inventore? Enim, vel!",
+  },
+];
